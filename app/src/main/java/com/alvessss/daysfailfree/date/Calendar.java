0c23dd51c21d82686696ca7f88dd77d73a2ed195
Alvessss <backtobluepill@gmail.com>
2022-07-09 01:43:23 -0300

    remove extra lines

--- app/src/main/java/com/alvessss/daysfailfree/date/Calendar.java
+++ app/src/main/java/com/alvessss/daysfailfree/date/Calendar.java
@@ -1,15 +1,12 @@
 package com.alvessss.daysfailfree.date;
 
-// using java.util.Calendar
-
 @SuppressWarnings("all")
 public class Calendar {
    /*
       That class is-like a interface that will provide all we need about Date in this app
     */
 
-   private static final java.util.Calendar
-      javaCalendar = java.util.Calendar.getInstance();
+   private static final java.util.Calendar javaCalendar = java.util.Calendar.getInstance();
 
    // Supported languages so far
    public static final int EN_US = 0;
@@ -28,19 +25,8 @@ public class Calendar {
          {"SEGUNDA", "TERÇA", "QUARTA", "QUINTA", "SEXTA", "SABADO", "DOMINGO"}                                                                    // PT-BR
    };
 
-   public static String getNameOfCurrentDay(int IDIOM){
-      return DAYS_NAMES[IDIOM][getCurrentDay()];
-   }
-
-   public static String getNameOfCurrentMonth(int IDIOM){
-      return MONTH_NAMES[IDIOM][getCurrentMonth()];
-   }
-
-   public static int getCurrentDay(){
-      return javaCalendar.get(java.util.Calendar.DAY_OF_WEEK);
-   }
-
-   public static int getCurrentMonth(){
-      return javaCalendar.get(java.util.Calendar.MONTH);
-   }
+   public static String getNameOfCurrentDay(int IDIOM){ return DAYS_NAMES[IDIOM][getCurrentDay()]; }
+   public static String getNameOfCurrentMonth(int IDIOM){ return MONTH_NAMES[IDIOM][getCurrentMonth()]; }
+   public static int getCurrentDay(){ return javaCalendar.get(java.util.Calendar.DAY_OF_WEEK); }
+   public static int getCurrentMonth(){ return javaCalendar.get(java.util.Calendar.MONTH); }
 }
