2dd8ead3045777551ce50d6522c9ef9a5962864a
Alvessss <backtobluepill@gmail.com>
2022-07-08 21:44:51 -0300

    Initial commit

---
+++ app/src/main/java/com/alvessss/daysfailfree/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alvessss.daysfailfree;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+// TODO: extract calendar data from some API to fill the Calendar View
+
+public class MainActivity extends AppCompatActivity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+   }
+}
